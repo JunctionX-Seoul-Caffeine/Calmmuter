13ec1aecea2bac07a1b5b96741142289bdb9f72e
YeonhyukKim <32633156+BranKein@users.noreply.github.com>
2021-05-23 03:26:34 +0900

    remove unused import

--- src/pages/AskBodyInfoPage.js
+++ src/pages/AskBodyInfoPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import StatusBar from "../components/StatusBar";
 import "../styles/AskBodyInfoPage.css";
 import "../styles/StatusBar.css";
