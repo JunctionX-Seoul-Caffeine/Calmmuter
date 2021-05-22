74bddd7ea408ba057bc301e85b8a9f2d7a1bf744
KIm, Hyeonseo <dodok8@gmail.com>
2021-05-23 00:52:31 +0900

    MainPage 만듦

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <div className="wrapper">
-        <Route path="/" component={MainPage} exact />
+        <Route path="/main" component={MainPage} exact />
         <Route path="/rate" component={RatePage} exact />
       </div>
     </div>
